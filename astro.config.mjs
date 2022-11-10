ab33d5bc999037614575cce7392b4055871e2145
Brazy <me@brazy.one>
2022-11-10 20:34:07 +0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  site: 'https://brazy.one',
+  integrations: [mdx(), sitemap(), tailwind()]
+});
