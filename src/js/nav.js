5aa5781b8025034c697267a1e2efe8e6440e3b3a
Brazy <me@brazy.one>
2022-11-14 17:31:02 +0300

    Remove unnecessary javascript

--- src/js/nav.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.link').forEach((link) => {
-  if (link.href === window.location.href) {
-    link.nextElementSibling.classList.toggle('active');
-  }
-  if (!(link.href === window.location.href)) {
-    link.nextElementSibling.classList.toggle('inactive');
-  }
-});
