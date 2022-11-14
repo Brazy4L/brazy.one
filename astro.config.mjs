059f7dc1fc34c81eb8c5a98c3096c3422d5d3b02
Brazy <me@brazy.one>
2022-11-14 20:25:45 +0300

    Add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://brazy.one',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
