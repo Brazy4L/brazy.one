8d283a4043a19d071fb28b4ec2ef5dfc297bc939
Vladislav Boichenko <me@brazy.one>
2022-11-20 17:58:01 +0300

    Change AboutMe

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Vladislav Boichenko';
-export const SITE_DESCRIPTION = 'Web Developer & UI/UX Designer';
+export const SITE_TITLE =
+  'Vladislav Boichenko | Web Developer & UI/UX Designer';
+export const SITE_DESCRIPTION =
+  'My name is Vladislav Boichenko. I am a web developer and UI/UX designer. I love art, music, tech and gaming.';
