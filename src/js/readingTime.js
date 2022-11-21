3473e89f9ec687353546512da31d39336f400894
Vladislav Boichenko <me@brazy.one>
2022-11-21 14:15:16 +0300

    Add reading time to articles

---
+++ src/js/readingTime.js
@@ -0,0 +1,9 @@
+const wordsPerMinute = 200;
+
+export default function readingTime(post) {
+  if (!post) return;
+  const removeTags = post.replace(/<\/?[^>]+(>|$)/g, '');
+  const wordsTotal = removeTags.split(/\s/g).length;
+  const time = Math.ceil(wordsTotal / wordsPerMinute);
+  return time < 2 ? `• ${time} minute to read` : `• ${time} minutes to read`;
+}
