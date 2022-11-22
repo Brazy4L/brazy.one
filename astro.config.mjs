a1c7cb27a24bd673f584d02d5502506f8e8e3a27
Vladislav Boichenko <me@brazy.one>
2022-11-23 00:37:13 +0300

    Add internal link to tech

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,9 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
+import prefetch from '@astrojs/prefetch';
 
-// https://astro.build/config
-import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://brazy.one',
-  integrations: [mdx(), sitemap(), tailwind(), partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  }), prefetch()]
+  integrations: [mdx(), sitemap(), tailwind(), partytown(), prefetch()],
 });
