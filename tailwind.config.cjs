cfd9311425ca5f2f69f4f48bdf86b464cd6214af
Vladislav Boichenko <me@brazy.one>
2022-11-26 14:06:40 +0300

    Fix transition

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        'cubic': 'cubic-bezier(0.24, 0.11, 0.27, 1)'
+      }
+    },
     fontFamily: {
       major: ['"Major Mono Display"', 'monospace'],
       train: ['"Train One"', 'monospace'],
