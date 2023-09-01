e8108d06fa63883787ef54db6cb65179cebf6ac3
Brazy <me@brazy.one>
2023-09-01 14:21:23 +0300

    Add header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        magnificent: '#fa00ff',
+        pretty: '#212025',
+        inaccessible: '#727272',
+      },
+    },
+  },
+  plugins: [],
 }
