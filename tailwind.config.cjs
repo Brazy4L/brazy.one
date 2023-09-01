281219f9e4a89df968bafcdc36b3696f9529cd59
Brazy <me@brazy.one>
2023-09-01 20:40:29 +0300

    Update timing function

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,9 @@ module.exports = {
         pretty: '#212025',
         inaccessible: '#959595',
       },
+      transitionTimingFunction: {
+        cubic: 'cubic-bezier(0.24, 0.11, 0.27, 1)',
+      },
     },
   },
   plugins: [],
