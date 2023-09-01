9a4debb2009c7febbbaa975ae6d2d4994b60dc43
Brazy <me@brazy.one>
2023-09-01 22:08:14 +0300

    Add 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from '@astrojs/tailwind'
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brazy.one',
   markdown: {
-    shikiConfig: { theme: 'dark-plus' },
+    shikiConfig: {
+      theme: 'dark-plus'
+    }
   },
-  integrations: [tailwind({ applyBaseStyles: false })],
-})
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    sitemap(),
+  ]
+});
