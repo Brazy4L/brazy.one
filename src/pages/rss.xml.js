0b757749567172b20cbeef3a1154141d26b26718
Brazy <me@brazy.one>
2023-09-02 13:06:36 +0300

    Add rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,17 @@
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+
+export async function GET(context) {
+  const posts = await getCollection('blog')
+  return rss({
+    title: "BRAZY's BLOG",
+    description: 'Tales about my journey in web development',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.slug}/`,
+    })),
+  })
+}
